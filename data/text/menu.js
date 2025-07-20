76e93f8aa186d6b37bbe5f8fbe230a3d59977021
arkana <arkana.faisal.r@gmail.com>
2025-07-20 16:31:48 +0700

    Update menu.js

--- data/text/menu.js
+++ data/text/menu.js
@@ -1,1 +1,31 @@
-kajshs
+const menuDatas = [
+  {
+    name: "cireng",
+    price: [ '3k' ],
+    stock: 'tersedia',
+    extras: [
+      {name: 'isi', type: "read", data: "ayam"}
+    ],
+    image: "cireng.png"
+  },
+  {
+    name: "mochi",
+    price: [ '2k' ],
+    stock: 'tersedia',
+    extras: [
+      {name: 'rasa', type: 'read', data: 'coklat'}
+    ],
+    image: "mochi.png"
+  },
+  {
+    name: 'es krim',
+    price: [ '3k', '4k'],
+    stock: 'tersedia',
+    extras: [
+      {name: 'ukuran', type: 'selectable', referencePrice: true, data: [ 'medium', 'big' ]},
+      {name: 'topping', type: 'selectable', data: [ 'original', 'coklat', 'stroberi' ]}
+    ]
+  }
+]
+
+
