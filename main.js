ebf60bd9e2f83a4eb8e73c0b85217f329a062efb
arkana <arkana.faisal.r@gmail.com>
2025-07-31 12:40:15 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 const template = document.getElementById('template-node')
 
 
-  
+
 const essentialData = {}
 mainFunction()
 
 async function mainFunction(){
   initiateEssentialData()
-  
+
   const dataUrl = "https://docs.google.com/spreadsheets/d/1HnxPabYY0sNdeSYl8dzEEHXCg-9f0Royk3DK0s_uDWo/export?format=csv"
   let data, errorMessage
   try{
@@ -17,7 +17,7 @@ async function mainFunction(){
   } catch(error){
     errorMessage = error.message
   }
-  
+
   document.getElementById('loading-indicator').classList.add('hidden')
   if(errorMessage){
     const errorNode = template.content.getElementById('error-menu').cloneNode(true)
@@ -31,7 +31,7 @@ async function mainFunction(){
     return
   }
 
-  
+
   let dataCleaned = []
   for(let i=0; i<data.length; i++){
     const current = data[i]
@@ -41,12 +41,12 @@ async function mainFunction(){
       essentialData.prefilledGform = current[2]
       essentialData.contactPerson = current[3].split(',').map(val => val.trim().replace(/\)/g, '').split('('))
       essentialData.noticeText = current[4]
-      
+
       initiateEssentialData(essentialData)
       continue
     }
     if(i < 4){continue}
-      
+
     let dataObject = {}
     dataObject.name = current[0]
     dataObject.stock = current[1]
@@ -58,15 +58,15 @@ async function mainFunction(){
       dataObject.image = imgUrl
     }
     dataObject.price = current[3].split(',').map(val => val.trim())
-    
+
     let extras = []
     for(j=4; j<current.length; j++){
       if(current[j] === '') continue
-      
+
       let extra = current[j].split('=')
       dataObject[extra[0].trim()] = extra[1].split('/').map(val => val.trim())
     }
-      
+
     dataCleaned.push(dataObject)
   }
     dataCleaned.sort((a,b) => {
@@ -74,10 +74,10 @@ async function mainFunction(){
       if(a.stock === 'tersedia' && b.stock === 'habis'){return -1}
       return 0
     })
-    
+
   initiateMainData(dataCleaned)
 }
-  
+
 function initiateEssentialData(data){
   if(data){
     localStorage.setItem('company', data.company)
@@ -87,12 +87,12 @@ function initiateEssentialData(data){
 
   document.getElementById('company').textContent = localStorage.getItem('company') || 'YourFood'
   document.getElementById('hero-text').textContent = localStorage.getItem('slogan') || 'Nanti Makan Apa? Pesan Sekarang!'
- 
+
   document.getElementById('notice-text').textContent = localStorage.getItem('noticeText')
   if(localStorage.getItem('noticeText') && localStorage.getItem('noticeText').trim() !== ''){
     document.getElementById('notice-text').parentElement.classList.remove('hidden')
   }
-  
+
   if(data && data.contactPerson[0] !== ''){
     data.contactPerson.forEach(item => {
       const newNode = template.content.getElementById('contact-node').cloneNode(true)
@@ -102,36 +102,36 @@ function initiateEssentialData(data){
         const linkWa = 'https://wa.me/' + item[1] + '?text=' + 'permisi%20kak%20' + item[0] + '...%0A'
         window.open(linkWa, '_blank')
       }
-      
+
      document.getElementById('contact-list').appendChild(newNode)
     })
   }
 }
-  
- 
+
+
 const menuList = document.getElementById("menu-list")
 const menuListNode = template.content.getElementById("menu-list-node")
 let menuListNodeWidth = (menuList.offsetWidth * 0.7) + 'px'
-  
+
 function initiateMainData(data){
   const fragment = document.createDocumentFragment()
   data.forEach(menuData => {
     const newNode = menuListNode.cloneNode(true)
     newNode.onclick = () => {changeMenuSelected(newNode, event)}
     newNode.classList.remove('hidden')
-  
+
     const nodeChildren = newNode.children
     nodeChildren[0].src = menuData.image
     nodeChildren[0].alt = 'gambar ' + menuData.name
     nodeChildren[0].style.maxWidth = menuListNodeWidth
     nodeChildren[1].textContent = menuData.name
     newNode.dataset['name'] = menuData.name
-  
+
     const detailsChildren = newNode.querySelector('#detail-text').children
-    detailsChildren[1].textContent = "stok: " + menuData.stock
+    detailsChildren[1].textContent = "statud: " + menuData.stock
     detailsChildren[0].textContent = "harga: " + menuData.price[0]
     newNode.dataset['price'] = menuData.price[0]
-    
+
     if(menuData.stock === 'habis'){
       newNode.classList.add('opacity-50')
       const button = nodeChildren[2].children[1]
@@ -139,31 +139,42 @@ function initiateMainData(data){
       button.classList.add('bg-green-500/30')
       button.onclick = null
     }
-  
+
     const keys = Object.keys(menuData)
     for(let i=4; i<keys.length; i++){
+      console.log(menuData[keys[i]][1])
+      if(!menuData[keys[i]][1] || (menuData[keys[i]][1] && menuData[keys[i]][1].trim() === '')){
+        const newDetail = template.content.getElementById('read-menu-detail').cloneNode()
+        newDetail.classList.remove('hidden')
+        newDetail.textContent = keys[i] + ': ' + menuData[keys[i]][0]
+        
+        nodeChildren[2].children[0].appendChild(newDetail)
+        continue
+      }
+      
+      
       const newDetail = template.content.getElementById('select-menu-detail').cloneNode(true)
       newDetail.classList.remove('hidden')
-      
+
       newDetail.children[0].textContent = keys[i] + ': '
       newDetailBtnContainer = newDetail.children[1]
       newDetail.id = 'select-menu-detail-' + i
-    
+
       for(let j=0; j<menuData[keys[i]].length; j++){
         const newSelectableDetail = newDetailBtnContainer.children[0].cloneNode()
         newSelectableDetail.classList.remove('hidden')
-        
+
         newSelectableDetail.textContent = menuData[keys[i]][j]
         newSelectableDetail.id = newNode.dataset['name'].replace(/\s+/g, "") + '-item-detail-' + keys[i] + '-' + j
         newDetailBtnContainer.appendChild(newSelectableDetail)
-      
+
         newSelectableDetail.onclick = () => {
           let oldSelected = newSelectableDetail.parentElement.querySelector('.selected')
           oldSelected.classList.remove('selected', 'bg-blue-500/50')
-          
+
           newSelectableDetail.classList.add('selected', 'bg-blue-500/50')
           newNode.dataset['extra' + i] = newSelectableDetail.textContent
-          
+
           if(i === 4 && menuData.price.length > 1 && menuData.price.length > newSelectableDetail.id.at(-1)){
             detailsChildren[0].textContent = "harga: " + menuData.price[newSelectableDetail.id.at(-1)]
             newNode.dataset['price'] = menuData.price[newSelectableDetail.id.at(-1)]
@@ -174,22 +185,22 @@ function initiateMainData(data){
       }
       newDetailBtnContainer.children[1].classList.add('selected', 'bg-blue-500/50')
       newNode.dataset['extra' + i] = menuData[keys[i]][0]
- 
+
       nodeChildren[2].children[0].appendChild(newDetail)
     }
-  
+
     newNode.dataset.extraLength = keys.length
     fragment.appendChild(newNode)
   })
-  
+
   menuList.appendChild(fragment)
   menuList.children[0].classList.add('border-3', 'border-[#FFC107]', 'menu-selected')
   const firstListDetail = menuList.children[0].children[2]
   firstListDetail.style.maxHeight = firstListDetail.scrollHeight + 100 + "px"
 }
-  
 
-  
+
+
 function changeMenuSelected(newSelected, event) {
   oldSelected = document.querySelector(".menu-selected")
   if(oldSelected){
@@ -197,13 +208,13 @@ function changeMenuSelected(newSelected, event) {
     oldSelected.children[2].style.maxHeight = 0
   }
   if(event.target.tagName !== 'BUTTON' && oldSelected === newSelected){return}
-  
+
   newSelected.classList.add('border-3', 'border-[#FFC107]', 'menu-selected')
   const detail = newSelected.children[2]
   detail.style.maxHeight = detail.scrollHeight + "px"
 }
-  
-  
+
+
 const cartItemList = document.getElementById("cart-item-list")
 const cartItemListNode = template.content.getElementById('cart-item-list-node')
 const cartNotif = document.getElementById("cart-notification")
@@ -212,28 +223,28 @@ function addOrder(element){
   const parent = element.parentElement
   const rootParent = parent.parentElement
   const itemDetail = parent.children[0].children
-  
+
   const newItem = cartItemListNode.cloneNode(true)
   newItem.classList.remove('hidden')
   const children = newItem.children[0].children
-  
+
   let itemSelectedData = parent.id + "-" + itemDetail[0].textContent.split(' ')[1] + "-"
   children[1].textContent = rootParent.dataset['name']
   children[2].textContent = rootParent.dataset['price']
   totalPrice.textContent = parseInt(totalPrice.textContent) + parseInt(rootParent.dataset['price']) + 'k'
-  
+
   const extraDetailNode = children[3]
   for(i=4;i<rootParent.dataset.extraLength;i++){
     const newDetail = extraDetailNode.cloneNode()
     newDetail.classList.remove('hidden')
-    
-    
+
+
     newDetail.textContent = rootParent.dataset['extra' + i]
     itemSelectedData += (rootParent.dataset['extra' + i] + '-')
-    
+
     newItem.children[0].appendChild(newDetail)
   }
-    
+
   const existItemList = document.getElementById(itemSelectedData)
   if(existItemList){
     existItemList.children[0].children[0].textContent = parseInt(existItemList.dataset.orderCount) + 1
@@ -243,40 +254,40 @@ function addOrder(element){
     cartNotif.textContent = (parseInt(cartNotif.textContent) + 1) + ' baru!'
     return
   }  
-    
+
   newItem.id = itemSelectedData
   newItem.dataset.orderCount = 1
   cartItemList.appendChild(newItem)
-  
+
   const hr = document.createElement('hr')
   cartItemList.appendChild(hr)
-    
+
   cartNotif.parentElement.classList.remove('hidden')
   cartNotif.textContent = (parseInt(cartNotif.textContent) + 1) + ' baru!'
 }
-  
+
 function removeItem(el){
   const parent = el.parentElement
   removedPrice = parseInt(parent.children[0].children[2].textContent)
   totalPrice.textContent = parseInt(totalPrice.textContent) - removedPrice + 'k'
-  
+
   parent.dataset.orderCount = parseInt(parent.dataset.orderCount) -1
   if(parseInt(parent.dataset.orderCount) === 0){
     parent.nextElementSibling.remove()
     parent.remove()
     return
   }
-  
+
   parent.children[0].children[0].textContent = parent.dataset.orderCount
 }
-  
+
 function makeOrder(){
   const allOrderNode = cartItemList.children
   if(allOrderNode.length === 0){return}
-  
+
   let allOrderData = []
   let allPriceData = []
-  
+
   for(i=0; i<allOrderNode.length; i+=2){
     current = allOrderNode[i]
     allOrderData.push(
@@ -286,24 +297,24 @@ function makeOrder(){
     )
     allPriceData.push(current.children[0].children[2].textContent)
   }
-    
+
   allOrderData = allOrderData.join('\n')
-  
-    
+
+
   let newPrefilledGform = ''
   const startIndex = essentialData.prefilledGform.indexOf("entry.")
-  
+
   const firstEntryStartIndex = essentialData.prefilledGform.indexOf('=', startIndex)
   const firstEntryEndIndex = essentialData.prefilledGform.indexOf('&entry', startIndex)
   const secondEntryStartIndex = essentialData.prefilledGform.indexOf('=', firstEntryEndIndex)
-    
+
   newPrefilledGform = essentialData.prefilledGform.slice(0, secondEntryStartIndex+1) + totalPrice.textContent // + data.prefilledGform.slice(secondEntryEndIndex, data.prefilledGform.length)
   newPrefilledGform = newPrefilledGform.slice(0, firstEntryStartIndex+1) + encodeURIComponent(allOrderData)  + newPrefilledGform.slice(firstEntryEndIndex, essentialData.prefilledGform.length)
-  
+
   window.location.href = newPrefilledGform
 }
-  
-  
+
+
 const shoppingCart = document.getElementById("shopping-cart")
 const main = document.getElementById("page-main")
 const footer = document.getElementById('page-footer')
@@ -316,10 +327,11 @@ function toggleCart(){
     window.scrollBy(0,-120)
     return
   }
-  
+
   main.classList.add("hidden")
   footer.classList.add('hidden')
   shoppingCart.classList.remove("hidden")
   cartNotif.parentElement.classList.add('hidden')
   cartNotif.textContent = 0
 }
+
